ee73d0d1b49dbb35a70c771013c4aeb2781c4c58
Nikhil <marati.nikhil9@gmail.com>
2026-01-05 17:16:48 +0530

    removed inbound

--- main.tf
+++ main.tf
@@ -50,14 +50,6 @@ resource "aws_security_group" "Mysecurity" {
     tags = {
         Name = "MYSG"
     }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "Inbound" {
-    security_group_id = aws_security_group.Mysecurity.id
-    cidr_ipv4 = "0.0.0.0/0"
-    from_port = 22
-    ip_protocol = "tcp"
-    to_port = 22
 } 
 
 
